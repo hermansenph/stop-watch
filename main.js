141eb2660c6e1ade85e1044bced859e3359af5b6
hermansenph <hermansenph@gmail.com>
2017-09-11 13:30:26 -0700

    Add function to display timer limit and flash red when timer has been reached #4

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ var $seconds = document.querySelector('#seconds')
 var $start = document.querySelector('#start')
 var $stop = document.querySelector('#stop')
 var $reset = document.querySelector('#reset')
+var $timeLimit = document.querySelector('#timelimit')
+var $timeLimitSet = document.querySelector('#settimelimit')
+var $stopWatch = document.querySelector('#stopwatch')
 var timer = 0
 
 function secondsPerSeconds() {
@@ -19,8 +22,33 @@ function stop() {
 
 function reset() {
   $seconds.textContent = 0
+  document.querySelector('body').classList.remove("alert")
+}
+
+function timeLimitCheck() {
+  setInterval(timeLimit, 50)
+}
+
+function timeLimit() {
+  var limit = document.querySelector('.limit')
+  if  (!limit) {
+    $stopWatch.appendChild(renderTimeLimit())
+  }
+  else {limit.textContent = '/' + $timeLimit.value}
+  if (limit.textContent === '/' + $seconds.textContent) {
+    clearInterval(timer)
+    document.querySelector('body').classList.add("alert")
+  }
+}
+
+function renderTimeLimit() {
+  var createLimit = document.createElement('p')
+  createLimit.textContent = '/' + $timeLimit.value
+  createLimit.classList.add("limit")
+  return createLimit
 }
 
 $start.addEventListener('click', secondsPerSeconds)
 $stop.addEventListener('click', stop)
 $reset.addEventListener('click', reset)
+$timeLimitSet.addEventListener('click', timeLimitCheck)
